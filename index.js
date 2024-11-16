6beabb89f6df93f431e97855e0836d9beed8f859
kyle <nojaim.k@gmail.com>
2024-11-16 16:03:15 -0500

    struggling with switching between home and about pages

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('<h1>this is a home page</h1>')
+    if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(content)
+        })
     }
 })
 
